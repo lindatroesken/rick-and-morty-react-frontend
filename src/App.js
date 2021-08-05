2b3bfab7767d5e959d58ac00c0c3500cd5eb421b
lindatrosken <linda.troesken@gmx.net>
2021-08-05 10:13:05 +0200

    Stand Feierabend am Mittwoch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header";
+import CharacterCard from "./components/CharacterCard";
+import response from './character-response.json'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const heading = "Character Gallery"
+    const characters = response.results
+    return (
+        <div>
+            <Header title={heading}/>
+            <main className="cardboard">
+                <CharacterCard character={characters[0]} />
+                <CharacterCard character={characters[1]} />
+                <CharacterCard character={characters[2]} />
+                <CharacterCard character={characters[3]} />
+                <CharacterCard character={characters[4]} />
+                <CharacterCard character={characters[5]} />
+                <CharacterCard character={characters[6]} />
+                <CharacterCard character={characters[7]} />
+                <CharacterCard character={characters[8]} />
+            </main>
+        </div>
+
+    );
 }
 
 export default App;
---
+++ src/components/CharacterCard.js
@@ -0,0 +1,13 @@
+import './CharacterCard.css'
+
+export default function CharacterCard(props){
+    return <div className="character-card">
+        <h1 className="character-card__title">{props.character.name}</h1>
+        <img className="character-card__image" src={props.character.image} alt={"Image of character "+ props.character.name}/>
+        <div className="character-card__properties ">
+            <div>Origin: {props.character.origin.name}</div>
+            <div>Species: {props.character.species}</div>
+            <div>Status: {props.character.status}</div>
+        </div>
+    </div>
+}
